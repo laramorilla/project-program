afd5ecaaa3a3aa2b02b01c945516a02769f74ade
FélixGómezR <gomezrodriguezjosefelix@gmail.com>
2017-11-27 21:00:35 +0100

    Eliminar todos los archivos

--- splc/wp-content/plugins/splcregistration/handleRegistration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function register_user(){}
-	$idRegistration = complete_registration($registration_info);
-	return $idRegistration;
-
-?>
--- splc/wp-content/plugins/splcregistration/splcOptions.php
+++
@@ -1,6 +0,0 @@
-<?php
-$emailsAdmin 		= array("localorganizer" => "splc2017@us.es", "paymentsupervisor" => "splc2017@viajeseci.es");
-$studentToken 		= "studentSPLC2017";
-$registrationpage 	= "registration";
-
-?>
