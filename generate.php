070fd297e7487d51a313b124f30180c6203d8725
laramorilla <32852642+laramorilla@users.noreply.github.com>
2018-01-10 19:47:35 +0100

    Add files via upload
    Funcionamiento de exportar a pdf

--- generate.php
+++ generate.php
@@ -1,6 +1,10 @@
 <?php
+
 require_once("../../../wp-config.php");
 require_once('config.inc.php');
+ob_end_clean();   
+header("Content-Encoding: None", true);
+
 
 // wp infos
 $home = get_option('home'); //e.g. www.example.com
@@ -53,28 +57,14 @@ if(isset($_GET['category'])){
             if ($layout == 1) $pdf->create_page(convert2iso($post->post_title."   ".$post->post_date), convert2iso($name." - ".$auth->user_email), convert2iso($post->post_content),true,$count++);
             /* Single page layout */
             else $pdf->create_page(convert2iso($post->post_title."   ".$post->post_date), convert2iso($name." - ".$auth->user_email), convert2iso($post->post_content));
-        }
+	   }
     }
+	
     $pdf->write_out();
-    
     /*Activate this line if you want email notification on download*/    
     //@mail($auth->user_email,"Download category ".$_GET['name'],"Hello, category ".$_GET['name']."\n has been downloaded from ".$url);
 }
 
-/*function is_utf8($string) {
-   // From http://w3.org/International/questions/qa-forms-utf-8.html
-   return preg_match('%^(?:
-         [\x09\x0A\x0D\x20-\x7E]            # ASCII
-       | [\xC2-\xDF][\x80-\xBF]            # non-overlong 2-byte
-       |  \xE0[\xA0-\xBF][\x80-\xBF]        # excluding overlongs
-       | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
-       |  \xED[\x80-\x9F][\x80-\xBF]        # excluding surrogates
-       |  \xF0[\x90-\xBF][\x80-\xBF]{2}    # planes 1-3
-       | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
-       |  \xF4[\x80-\x8F][\x80-\xBF]{2}    # plane 16
-   )*$%xs', $string);
-}
-*/
 function is_utf8($str) {
 if(function_exists("mb_detect_encoding"))
 {
