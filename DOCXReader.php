81a66ab9e858670965adfc44ca1d57e0c7384b09
samusfc <samgonzul@hotmail.com>
2018-01-11 13:10:53 +0100

    feat: Añadida función que lee un fichero docx
    Se ha conseguido hacer una función que lee un fichero docx.

--- DOCXReader.php
+++ DOCXReader.php
@@ -5,13 +5,49 @@ class DOCXReader{
 
 // Open the file
 
-    public function readFile($file){
-        $lineas = file($file);
-        if($lineas === true) {
-            $this->parse();
-        } else {
-            throw new Exception("Failed to open $file with zip error code: $lineas");
-        }
+    function read_file_docx($filename){
+        
+        $striped_content = '';
+        $content = '';
+        
+        if(!$filename || !file_exists($filename)) return false;
+        
+        $zip = zip_open($filename);
+        
+        if (!$zip || is_numeric($zip)) return false;
+        
+        while ($zip_entry = zip_read($zip)) {
+            
+            if (zip_entry_open($zip, $zip_entry) == FALSE) continue;
+            
+            if (zip_entry_name($zip_entry) != "word/document.xml") continue;
+            
+            $content .= zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+            
+            zip_entry_close($zip_entry);
+        }// end while
+        
+        zip_close($zip);
+        
+        //echo $content;
+        //echo "<hr>";
+        //file_put_contents('1.xml', $content);
+        
+        $content = str_replace('</w:r></w:p></w:tc><w:tc>', " ", $content);
+        $content = str_replace('</w:r></w:p>', "\r\n", $content);
+        $striped_content = strip_tags($content);
+        
+        return $striped_content;
+    }
+    $filename = "filepath";// or /var/www/html/file.docx
+    
+    $content = read_file_docx($filename);
+    if($content !== false) {
+        
+        echo nl2br($content);
+    }
+    else {
+        echo 'Couldn\'t the file. Please check that file.';
     }
     
 // extract the shared string and the list of sheets
