22c82f95687243041414166cdeeea0a48de14238
Samuel <samgonzul@hotmail.com>
2018-01-14 21:26:47 +0100

    doc: añadidas funcionalidades

---
+++ generate.php
@@ -0,0 +1,111 @@
+<?php
+
+require_once("../../../wp-config.php");
+require_once('config.inc.php');
+ob_end_clean();   
+header("Content-Encoding: None", true);
+
+
+// wp infos
+$home = get_option('home'); //e.g. www.example.com
+$siteurl = get_option('siteurl'); //e.g. www.example.com/wordpress - the fisical install directory
+$b_name = get_option('blogname');
+$adm_email = get_option('admin_email'); 
+
+if(isset($_GET['post'])){
+    $layout = 2; //Choices 1 for chapter or 2 for single page layout
+    $id = $_GET['post'];
+    $post = get_post($id);
+    if(!preg_match($exclude_tag,$post->post_content)){    
+
+        $auth = get_userdata($post->post_author);
+        $p_link = get_permalink($post->ID);
+    
+    
+        if($auth->first_name || $auth->last_name) $name = $auth->first_name." ".$auth->last_name;
+        else $name = $auth->user_nicename;
+        
+        /*Activate this line if you want email notification on download*/    
+        //@mail($auth->user_email,"Download pdf ".$post->post_title,"Hello, post ".$post->post_title."\n has been downloaded from ".$p_link); 
+        $pdf=new PDF(convert2iso($b_name." - ".$post->post_title), convert2iso($b_name." - ".$home), convert2iso("Post url: ".$p_link." - Copyright ".$name." ".$auth->user_email), convert2iso($name));
+        
+        /* Chapter page layout */        
+        if ($layout == 1) $pdf->create_page(convert2iso($post->post_title."   ".$post->post_date), convert2iso($name." - ".$auth->user_email), convert2iso($post->post_content),true,1);
+        /* Single page layout */
+        else $pdf->create_page(convert2iso($post->post_title."   ".$post->post_date), convert2iso($name." - ".$auth->user_email), convert2iso($post->post_content));
+        
+        $pdf->write_out();
+    }else{  echo "Error: Unable to export this post."; }
+}
+
+if(isset($_GET['category'])){
+    $layout = 1; //Choices 1 for chapter or 2 for single page layout
+    $myposts = get_posts('numberposts=10000&category='.$_GET['category']);
+        
+    $pdf=new PDF(convert2iso($b_name."-".$_GET['name']), convert2iso($b_name." - ".$home), convert2iso("Copyright ".$b_name." ".$adm_email), convert2iso($b_name));
+    $count = 1; /* Chapter count */
+    /* Coversheet page */
+    $pdf->create_page(convert2iso($b_name), convert2iso($_GET['name']." archive"),"");
+    foreach($myposts as $post){
+        if(!preg_match($exclude_tag,$post->post_content)){    
+            $auth = get_userdata($post->post_author);
+            $p_link = get_permalink($post->ID);
+
+            if($auth->first_name || $auth->last_name) $name = $auth->first_name." ".$auth->last_name;
+            else $name = $auth->user_nicename;            
+            /* Chapter page layout */        
+            if ($layout == 1) $pdf->create_page(convert2iso($post->post_title."   ".$post->post_date), convert2iso($name." - ".$auth->user_email), convert2iso($post->post_content),true,$count++);
+            /* Single page layout */
+            else $pdf->create_page(convert2iso($post->post_title."   ".$post->post_date), convert2iso($name." - ".$auth->user_email), convert2iso($post->post_content));
+	   }
+    }
+	
+    $pdf->write_out();
+    /*Activate this line if you want email notification on download*/    
+    //@mail($auth->user_email,"Download category ".$_GET['name'],"Hello, category ".$_GET['name']."\n has been downloaded from ".$url);
+}
+
+function is_utf8($str) {
+if(function_exists("mb_detect_encoding"))
+{
+	if (mb_detect_encoding($str, "auto") == "UTF-8")
+		return true;
+	else return false;
+}else
+{
+    $c=0; $b=0;
+    $bits=0;
+    $len=strlen($str);
+    for($i=0; $i<$len; $i++){
+        $c=ord($str[$i]);
+        if($c > 128){
+            if(($c >= 254)) return false;
+            elseif($c >= 252) $bits=6;
+            elseif($c >= 248) $bits=5;
+            elseif($c >= 240) $bits=4;
+            elseif($c >= 224) $bits=3;
+            elseif($c >= 192) $bits=2;
+            else return false;
+            if(($i+$bits) > $len) return false;
+            while($bits > 1){
+                $i++;
+                $b=ord($str[$i]);
+                if($b < 128 || $b > 191) return false;
+                $bits--;
+            }
+        }
+    }
+    return true;
+	}
+}
+function convert2iso($string) {
+
+    if (is_utf8($string)) {
+		return utf8_decode($string);
+    } else {
+        return $string;
+    }
+	//return @utf8_decode($string);
+}
+    
+?>
---
+++ program-manager.php
@@ -0,0 +1,134 @@
+<?php
+/*
+Plugin Name: Program manager plugin
+*/
+
+require('XLSXReader.php');
+require('pmanagerlib.php');
+
+/*Inicio funcion exportar a pdf*/
+require_once("pdf_class.php");
+
+function post_to_pdf($content){
+    global $wp_query;
+    include("config.inc.php");
+    if(!preg_match($exclude_tag,$content))
+    {
+        $siteurl = get_option("siteurl");
+        $id = $wp_query->post->ID;
+        $temp = preg_replace("[##SITEURL##]",$siteurl, $html_post_code);
+        $temp = preg_replace("[##GLOBALID##]",$id ,$temp);
+        $check = preg_match($export_tag, $content);
+        if(!$default_post_action)
+        {
+            if($check)
+            {
+                $content = preg_replace($export_tag, $temp, $content);
+            }
+        }
+        else
+        {
+            if(!$check)
+            {
+                $content = $content." ".$temp;
+            }
+            else
+            {
+                $content = preg_replace($export_tag, $temp, $content);
+            }
+        }
+    }
+    return $content;
+}
+
+function archive_to_pdf($content, $category=null){
+    include("config.inc.php");
+    global $wpdb;
+    if ($category && $export_category) {
+        $siteurl = get_option("siteurl");
+        $temp = preg_replace("##CATEGORYID##", $category->cat_ID, $html_category_code);
+        $temp = preg_replace("##CATEGORYNAME##", $category->cat_name, $temp);
+        $temp = preg_replace("##SITEURL##",$siteurl,$temp);
+        return $content." ".$temp;
+    }else return $content;
+}
+
+add_filter("the_content", "post_to_pdf");
+add_filter("list_cats", "archive_to_pdf",10,2);
+
+/*Fin funcion exportar a pdf*/
+
+add_shortcode( 'write_long_program', 'write_program' );
+add_action( 'wp_enqueue_scripts', 'my_plugin_register_scripts' );
+
+function my_plugin_register_scripts(){
+     wp_register_script('my-script',plugins_url( '/my-script.js', __FILE__ ), false, '1.0', 'all' );
+     wp_register_style( 'my-style', plugins_url( '/my-style.css', __FILE__ ), false, '1.0', 'all' );
+}
+
+function write_program(){
+	date_default_timezone_set('UTC');
+	wp_enqueue_style( 'my-style' );
+	wp_enqueue_script( 'my-script' );
+
+	$xlsx = new XLSXReader('./wp-content/plugins/project-program/programme-main-v9.xlsx');
+  $xlsxPRE = new XLSXReader('./wp-content/plugins/project-program/programme-pre-v8.xlsx');
+
+	$days = array(
+    0 => "Monday",
+    1 => "Tuesday",
+		2 => "Wednesday",
+		3 => "Thursday",
+		4 => "Friday",
+	);
+
+	echo "<div class=\"w3-bar w3-black\">";
+  $j = 0;
+	foreach($days as $day) {
+    if($j==0){
+      echo  "<button class=\"w3-bar-item w3-button\" style=\"background:#000\" onclick=\"openTab('".$day."')\">".$day."</button>";
+    }else{
+      echo  "<button class=\"w3-bar-item w3-button\" onclick=\"openTab('".$day."')\">".$day."</button>";
+    }
+    $j++;
+
+	}
+	$j = 0;
+
+	foreach($days as $day) {
+    #Print tabular program
+    if($j==0){
+		      echo "<div id=\"". $day ."\" class=\"tab\">";
+	  }else{
+			     echo "<div id=\"". $day ."\" class=\"tab\" style=\"display:none\">";
+		}
+
+    #process Data from file
+    if($day == "Monday" || $day == "Tuesday"){
+      $daySheet = $xlsxPRE->getSheet($day);
+      processPreDay($daySheet->getData());
+
+    }else{
+      $daySheet = $xlsx->getSheet($day);
+      processDay($daySheet->getData());
+
+    }
+		echo '</div>';
+		$j++;
+    }
+?>
+	<iframe name="suscribirse" style="display:none;"></iframe>
+	<form action="funcionmail.php" method="post" target="suscribirse">
+    Introduce aquí tu email si quieres saber cuando se ha modificado el programa!:  <input type="text" name="email" /><br />
+    <input type="submit" name="submit" value="¡Enviarme!" />
+</form>
+	<?php
+
+
+}
+?>
+
+
+
+
+
